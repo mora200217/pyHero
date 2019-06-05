057031f0381fac5e1350f4f4d8615e0b09ed9eb8
Andres Morales <mora200217@hotmail.com>
2019-06-05 15:23:10 -0500

    Eliminado atributo de sprite. Velocidad Creada

--- Nota.py
+++ Nota.py
@@ -10,11 +10,11 @@ INICIAL_VERTICAL = 180 # Posición
 
 # TODO: Convertir el objeto a un sprite de pg 
 
-class Nota(pg.sprite.Sprite):
+class Nota():
     '''
         CLASE_ Nota. Encargada de generar las notas del juego.
     '''
-    def __init__(self, color, pos_inicio, width, height):
+    def __init__(self, color, pos_inicio, width, height, division):
        """
         Claves de Color: 
         0 -> amarillo
@@ -22,7 +22,7 @@ class Nota(pg.sprite.Sprite):
         2 -> verde
         3 -> azul
        """
-       pg.sprite.Sprite.__init__(self)
+
        # Definicion de Variables
        self.height = 70 
        self.type = color # El identificador de color se guarda como tipo de nota, para mejorar lectura  
@@ -30,12 +30,11 @@ class Nota(pg.sprite.Sprite):
        self.factor = 1.1 # Factor de escala de transformación. 
        self.pos = Vector(pos_inicio, INICIAL_VERTICAL)
        # self.pos = [pos_inicio,INICIAL_VERTICAL]
-       temp_x = width / 2 - pos_inicio
-       temp_f = abs(temp_x) / (temp_x)
-       pos_i = Vector(pos_inicio, INICIAL_VERTICAL)
-       pos_f = Vector(width / 2 + temp_x * 2, height)
-       self.vel = pos_f - pos_i # Pixeles por frame
-       self.vel.normalize()
+       
+       self.temp_f = Vector((145 + division *  color), height - 70)
+       self.temp_i = Vector(pos_inicio, INICIAL_VERTICAL)
+       
+       self.vel  =(self.temp_f - self.temp_i).normalize()
        self.fuente_imagen = pg.image.load(os.path.join('assets/notas', 'nota_' + COLORES[color] + '.png')) 
        self.dimensions = (int(self.height),int(self.ratio * self.height)) 
        self.image = pg.transform.scale(self.fuente_imagen, self.dimensions)
@@ -54,7 +53,7 @@ class Nota(pg.sprite.Sprite):
         '''
         Genera el movimiento de la nota
         '''
-        self.pos +=  self.vel.normalize()
+        self.pos +=  self.vel
 
     def get_type(self):
         '''
